134ad3f9a4f10b64917ddaf1abc1a26743abd38d
stevewest <steve.west@veinteractive.com>
2014-09-23 09:32:51 +0100

    Adds missing interface methods

--- tests/stubs/EntityDefinitionInterfaceStub.php
+++ tests/stubs/EntityDefinitionInterfaceStub.php
@@ -5,4 +5,29 @@ namespace Ve\LogicProcessor;
 class EntityDefinitionInterfaceStub implements EntityDefinitionInterface
 {
 
+	/**
+	 * Should return true if the entity contains the given property.
+	 *
+	 * @param string $name
+	 *
+	 * @return bool
+	 */
+	public function hasProperty($name)
+	{
+		// TODO: Implement hasProperty() method.
+	}
+
+	/**
+	 * Should return an array where the keys are the names of properties and the value is one or more types that apply
+	 * to the property.
+	 *
+	 * ['foo' => TypeEnum::$STRING, 'bar' => [TypeEnum::$STRING, TypeEnum::$NUMBER]]
+	 *
+	 * @return array
+	 */
+	public function getProperties()
+	{
+		// TODO: Implement getProperties() method.
+	}
+
 }
